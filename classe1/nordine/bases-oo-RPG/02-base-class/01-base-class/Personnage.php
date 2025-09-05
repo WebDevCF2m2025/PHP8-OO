c2325c1073df4daafb9a5ac5a83fb8299fff2f23
Nono1050 <abdelkadernordine@hotmail.com>
2025-09-05 07:11:30 +0200

    second commit

---
+++ classe1/nordine/bases-oo-RPG/02-base-class/01-base-class/Personnage.php
@@ -0,0 +1,16 @@
+<?php
+
+// La classe est une usine qui va permettre de créer des objets
+// de son type via une instanciation
+// La classe doit avoir le même nom que le fichier
+
+class Personnage{
+    // Propriétés (Attributs)
+    // ce sont des variables propres à la classe.
+    // Elles ont 3 vivibilités : public, private ou protected
+    public $the_name;
+    // Constantes
+    // Méthodes
+
+   
+    }
---
+++ classe1/nordine/bases-oo-RPG/03-base-class-visibilite-typage/02-base-class/01-base-class/Personnage.php
@@ -0,0 +1,22 @@
+<?php
+
+// La classe est une usine qui va permettre de créer des objets
+// de son type via une instanciation
+// La classe doit avoir le même nom que le fichier
+
+class Personnage{
+    // Propriétés (Attributs)
+    // ce sont des variables propres à la classe.
+    // Elles ont 3 vivibilités : public, private ou protected
+    public string $the_name;  // publique, typage en string
+
+    public ?string $the_surname="unknown"; // publique, string ou null (?string) avec valeur par default.
+
+    public ?string $the_walking_dead=null=5; // publique ,string ou null (?string)
+
+
+    // Constantes
+    // Méthodes
+
+   
+    }
